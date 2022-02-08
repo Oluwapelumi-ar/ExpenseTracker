ce02f86978f6e4f94b2a537a0607876e9ef1904e
Bluebird2000 <lateefahmad3868@gmail.com>
2022-02-08 04:36:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(),
+      title:'Car Insurance',
+      amount:20.56
+    },
+    {
+      date: new Date(),
+      title:'Life Insurance',
+      amount:20.56
+    },
+    {
+      date: new Date(),
+      title:'New Desk',
+      amount:20.56
+    },
+    {
+      date: new Date(),
+      title:'Cars',
+      amount:20.56
+    },
+    
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h2>Let's get started!</h2>
+      </div>
+      <Expenses items={expenses}/>
     </div>
   );
 }
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+const [title,setTitle]= useState(props.title)
+
+const clickHandler=() =>{
+    setTitle('Update');
+    console.log(title)
+}
+    return (
+    <div className="expense-item" >  
+        <ExpenseDate date={props.date}></ExpenseDate>
+        <div className="expense-item__description"> 
+            <h2>{title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+            <button onClick={clickHandler}>Change Text</button>
+        </div>
+    </div>
+    )
+}
+export default ExpenseItem
+
+
