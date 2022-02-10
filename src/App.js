041d6026975a655e623d7a8909a20bfb77055664
Shukurah Bakare <shukurahbakare@outlook.com>
2022-02-10 16:53:01 +0100

    Rendered dynamic data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 
-import ExpenseItem from "./components/Expenses/ExpenseItem";
+import { useState } from "react/cjs/react.development";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const expensesDummyData = [
+  {
+    date: new Date(),
+    title:'Car Insurance',
+    amount:20.56
+  },
+  {
+    date: new Date(),
+    title:'Life Insurance',
+    amount:20.56
+  },
+  {
+    date: new Date(),
+    title:'New Desk',
+    amount:20.56
+  },
+  {
+    date: new Date(),
+    title:'Cars',
+    amount:20.56
+  },
+  
+]
 
 function App() {
-  const expenses = [
-    {
-      date: new Date(),
-      title:'Car Insurance',
-      amount:20.56
-    },
-    {
-      date: new Date(),
-      title:'Life Insurance',
-      amount:20.56
-    },
-    {
-      date: new Date(),
-      title:'New Desk',
-      amount:20.56
-    },
-    {
-      date: new Date(),
-      title:'Cars',
-      amount:20.56
-    },
-    
-  ]
+  const [expenses, setExpenses] = useState(expensesDummyData)
+
+  const addExpenseHandler = expense => {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    })
+  };
   return (
     <div>
       <div>
-        <h2>Let's get started!</h2>
+        <NewExpense onAddExpense={addExpenseHandler}/>
       </div>
       <Expenses items={expenses}/>
     </div>
